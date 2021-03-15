e22bdf5b01c15204a6b9873820f30e3a215a0acb
HelenArkh <alena.arkh@gmail.com>
2021-03-15 19:02:49 +0300

    Введение

---
+++ Module14/Module14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+           {
+               1,
+               "Сергей ",
+               "Андрей ",
+               300,
+           };
+
+            foreach (var stringObject in objects.Where(o => o is string).OrderBy(o => o))
+                Console.WriteLine(stringObject);
+        }
+    }
+}
